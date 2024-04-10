817d8e60a54bd71735551d6c76793299c73e3ffa
DFS_Jose_Costa <jose.costa@doverfs.com>
2024-04-09 23:26:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoUpdate;
+
+namespace AutoUpdate
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // setup the auto update, and exit if there was an update
+            // change the line below to your github repo where "AutoUpdate" is (YourAppName)/(YourRepoName)
+            Updater.GitHubRepo = "/Zenxyo1/AutoUpdate";
+            if (Updater.AutoUpdate(args))
+                return;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
